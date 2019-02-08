dc61faddf44e7c7a13dab9d9d5fc93b5c9baeafd
MadBadgerLab <47367216+MadBadgerLab@users.noreply.github.com>
2019-02-09 00:48:33 +0100

    Update Config-Server.ps1

--- Config-Server.ps1
+++ Config-Server.ps1
@@ -1,21 +1,31 @@
 ## Server config
-# Parameter
-$IP = ""
-$SubnetMask = ""
-$Gateway = ""
-$Hostname = ""
+Function Config-Server{
 
 
+    # Parameter
+    $IP = "10.0.0.180"
+    $Hostname = "MadLabDC1"
+    #$SubnetMask = "255.255.255.0"
+    $Gateway = "10.0.0.138"
+    $DnsServer1 = "10.0.0.180"
+    $DnsServer2 = "10.0.0.181"
+    $DomainCred = Get-Credential
 
-# IP
-New-NetIPAddress –InterfaceAlias “Wired Ethernet Connection” –IPv4Address “192.168.0.1” –PrefixLength 24 -DefaultGateway 192.168.0.254
+    # IP
+    New-NetIPAddress –InterfaceAlias “Wired Ethernet Connection” –IPv4Address $IP –PrefixLength 24 -DefaultGateway $Gateway
 
-# DNS
-Set-DnsClientServerAddress -InterfaceAlias “Wired Ethernet Connection” -ServerAddresses 192.168.0.1, 192.168.0.2
+    # DNS
+    Set-DnsClientServerAddress -InterfaceAlias “Wired Ethernet Connection” -ServerAddresses $DnsServer1, $DnsServer2
 
-# Set Hostname
-$name = (Get-WmiObject -query "Select * from Win32_SystemEnclosure").SMBiosAssetTag
-$computername = (Get-WmiObject Win32_ComputerSystem)
-$computername.Rename($name)
+    # Set Hostname
+    Rename-Computer -NewName $Hostname -Verbose #-ComputerName $RemoteComputer
 
-Rename-Computer -ComputerName -NewName -Verbose
+    # Join Domain
+    Add-Computer -DomainName "MadBadger.lab" -Credential $DomainCred
+
+
+}
+
+
+# Lage en liste over maskiner
+If(£name){dytt-config}
